4d455b7d56480de1107e304c171bd0a44dffe364
lee308812 <lee308812@gmail.com>
2021-02-24 01:24:09 +0900

    PriorityQueue의 STL버전 추가

---
+++ Practice/PriorityQueue/STLVER.cpp
@@ -0,0 +1,19 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <functional>
+using namespace std;
+
+priority_queue<int> maxpq;
+priority_queue<int, vector<int>, greater<int>> minpq;
+
+int main()
+{
+	maxpq.push(10);
+	minpq.push(50);
+
+	return 0;
+}
+*/
